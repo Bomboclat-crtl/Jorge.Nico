ffa32b13e424481f187fde1456a2d5d3c6a858b8
JORGE N. <dezaorihuela@gmail.com>
2026-04-23 16:25:29 -0500

    aaa

--- js/calculadora.js
+++ js/calculadora.js
@@ -29,6 +29,7 @@ document.addEventListener("DOMContentLoaded", function(){
                 res = nume1/nume2;
                 if(res<=0)
                     resultado.textContent = "ta mal"
+                return
                 }
                 break;
             default:
